74454cd3829bdc25af68814571359264b26648cc
Klebitz12890 <97742308+Klebitz12890@users.noreply.github.com>
2022-07-31 13:04:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ global.APIs = { // API Prefix
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
   'https://neoxr-api.herokuapp.com': 'yntkts',
-  'https://api.xteam.xyz': 'apikeylu',
+  'https://api.xteam.xyz': '94de078e667ef757',
   'https://melcanz.com': 'apikeylu',
   'https://api.lolhuman.xyz': 'apikeylu',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
